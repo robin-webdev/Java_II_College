c914bb716c1c86177d4c276ad92a55b7e0ff109d
robin-webdev <robin.webdev.me@gmail.com>
2026-03-04 22:08:05 +0200

    sdfdsfds

---
+++ src/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+    public String name;
+    public int age;
+    public int id;
+
+    public Person(String name, int age, int id, String plan, int months) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
